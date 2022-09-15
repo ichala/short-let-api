ffdc7d48ff24a4c95c026ae7def919b5c3ad14c8
Ali Jendoubi <alijandhd@gmail.com>
2022-09-15 12:03:53 +0100

    Fix Linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,31 @@
 class ApplicationController < ActionController::API
+  def jwt_key
+    ENV.fetch('JWT_KEY')
+  end
 
-    def jwt_key
-        jwt_key= ENV["JWT_KEY"] 
-    end
+  def issue_token(user)
+    JWT.encode({ user_id: user.id }, jwt_key, 'HS256')
+  end
 
-    def issue_token(user)
-        JWT.encode({user_id: user.id}, jwt_key, "HS256")
-    end
+  def decoded_token
+    JWT.decode(token, jwt_key, true, { algorithm: 'HS256' })
+  rescue StandardError => e
+    [{ error: e }]
+  end
 
-    def decoded_token
-        begin
-            JWT.decode(token, jwt_key, true, { :algorithm => 'HS256' })
-        rescue => exception
-            [{error: "Invalid Token"}]
-        end    
-    end
+  def token
+    request.headers['Authorization'].split.last
+  end
 
-    def token
-        request.headers["Authorization"].split(' ').last
-    end
+  def user_id
+    decoded_token.first['user_id']
+  end
 
-    def user_id
-        decoded_token.first["user_id"]
-    end
+  def current_user
+    User.find_by(id: user_id)
+  end
 
-    def current_user
-        user ||= User.find_by(id: user_id)
-    end
-
-    def logged_in?
-        !!current_user
-    end
+  def logged_in?
+    !!current_user
+  end
 end
