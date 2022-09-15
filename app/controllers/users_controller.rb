cc2c173b540281ff0b4833a7c56810dee8959e34
Ali Jendoubi <alijandhd@gmail.com>
2022-09-15 11:13:53 +0100

    Add routes to test authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
-end
+    def create
+      user = User.new(user_params)
+      if user.save
+        token = issue_token(user)
+        render json: {user: UserSerializer.new(user), jwt: token}
+      else
+        if user.errors.messages
+          render json: {error: user.errors.messages}
+        else
+          render json: {error: "User could not be created. Please try again"}
+        end
+      end
+    end
+
+    private
+    
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  get "/authorized", to: "sessions#show"
+  # get "/dashboard", to: "users#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
