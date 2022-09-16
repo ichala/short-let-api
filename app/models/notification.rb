83020b46329de9772a83c955d0b6e00ae197c470
Aimal Amiri <meaimal@hotmail.com>
2022-09-16 13:13:58 +0430

    Add relationship between users and notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :reservation
+  belongs_to :recipient, class_name: 'User'
+  belongs_to :admin, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
   has_many :hall, foreign_key: :created_by_id
   has_many :reservations
+  has_many :notified, class_name: 'Notification', foreign_key: :recipient_id
+  has_many :notifier, class_name: 'Notification', foreign_key: :admin_id
 end
