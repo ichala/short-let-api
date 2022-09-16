732aa5d08f90b1a901b7d7da80c91231e5e21cdf
Aimal Amiri <meaimal@hotmail.com>
2022-09-16 13:26:12 +0430

    Add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :notified, class_name: 'Notification', foreign_key: :recipient_id
   has_many :notifier, class_name: 'Notification', foreign_key: :admin_id
+
+  validates :first_name, presence: true, length: { max: 255 }
+  validates :last_name, presence: true, length: { max: 255 }
+  validates :email, presence: true, length: { max: 255 }
 end
