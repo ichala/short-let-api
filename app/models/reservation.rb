fc111007092f47ce01fce9d7c561564387978c33
Aimal Amiri <meaimal@hotmail.com>
2022-09-16 13:25:53 +0430

    Add validation to reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :hall
   belongs_to :user
   has_many :notifications
+
+  validates :reserve_date, presence: true
+  validates :status, presence: true, length: { max: 20 }
 end
