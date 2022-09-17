8b0e42d2333ce602087be4b175b51c5732791641
Ali Jendoubi <alijandhd@gmail.com>
2022-09-17 10:20:43 +0100

    Fix Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,6 @@
 class Notification < ApplicationRecord
-  belongs_to :reservation
+  belongs_to :reservation,class_name: 'Reservation', foreign_key: :reserve_id
   belongs_to :recipient, class_name: 'User'
   belongs_to :admin, class_name: 'User'
-
   validates :text, presence: true
 end
