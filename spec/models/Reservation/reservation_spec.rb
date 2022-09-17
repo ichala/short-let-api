92fa999b913a7cafd5de2c55586687abe60d72c5
EidHachem <eidhachem1@gmail.com>
2022-09-17 21:29:34 +0300

    Add test for presence of reservation date

---
+++ spec/models/Reservation/reservation_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  @user = User.create(first_name: 'Eid', last_name: 'Hachem',role: "admin", email: 'test@test.com', password: '123456')
+  @hall = Hall.create(name: 'Main', capacity: 100, cost: 50.5, image: 'https://testimage', description: 'Small', created_by_id: @user.id)
+  subject { Reservation.new(reserve_date: Date.new, status: 'Pending', user_id: @user, hall_id: @hall) }
+  before { subject.save }
+
+  it 'reservation date should be present' do
+    subject.reserve_date = nil
+    expect(subject).to_not be_valid
+  end
+end
