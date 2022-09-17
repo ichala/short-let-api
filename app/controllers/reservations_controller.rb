d363eeb4ff639ae6dbe8ee58d138358d7471e1ff
Ali Jendoubi <alijandhd@gmail.com>
2022-09-17 19:12:28 +0100

    Add get and post actions for pending reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,32 @@
 class ReservationsController < ApplicationController
+    #Get All Reservations With Status Pending
+    def get_pending_reservation_admin
+        if admin?
+            pending_reservations = Reservation.all.where(status:'Pending')
+            render json: pending_reservations, each_serializer: ReservationSerializer
+        else
+            render json: { error: 'Not Allowed' }, status: :unauthorized
+        end
+    end
+
+    #Update a Reservation Based On Admin Decision
+    def handle_pending_reservation_admin
+        if admin?
+            reservation = Reservation.find_by(id: action_params[:reservation_id].to_i)
+            raise ActiveRecord::RecordNotFound if !reservation
+            if action_params[:decision] == 'Confirm'
+            accept_reservation(reservation)
+            elsif action_params[:decision] == 'Refuse'
+            refuse_reservation(reservation)
+             end
+             else
+            render json: { error: 'Not Allowed' }, status: :unauthorized
+             end
+    end
+
+  
+
+    def action_params
+        params.permit(:reservation_id, :decision, :text)
+      end
 end
