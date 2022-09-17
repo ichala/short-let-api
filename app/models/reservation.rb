e8107adf0d15edefbfe5868f30c436b68de2c352
Ali Jendoubi <alijandhd@gmail.com>
2022-09-17 10:21:25 +0100

    Fix Reservation Model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :hall
   belongs_to :user
-  has_many :notifications, dependent: :delete_all
-
+  has_many :notifications, class_name: 'Notification', foreign_key: :reserve_id, dependent: :delete_all
   validates :reserve_date, presence: true
   validates :status, presence: true, length: { maximum: 20 }
 end
