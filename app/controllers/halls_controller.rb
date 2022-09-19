0ac62beed1e9bfd9d8e4022d5393ef32219a3a55
Ali Jendoubi <alijandhd@gmail.com>
2022-09-19 16:07:29 +0100

    Create Halls Crud methods

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -1,2 +1,85 @@
 class HallsController < ApplicationController
+  # Gett all halls
+  def halls_list
+    if admin?
+      render json: Hall.all, each_serializer: HallSerializer
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
+  end
+
+  # Delete Hall by id
+  def delete_hall
+    if admin?
+      hall = Hall.find(delete_params[:hall_id])
+      raise ActiveRecord::RecordNotFound unless hall
+
+      check_reservations(hall)
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
+  end
+
+  # Create New Hall
+  def add_hall
+    if admin?
+      hall = current_user.hall.new(create_params)
+      if hall.save
+        render json: { created: hall }
+      else
+        render json: { error: 'Bad Request' }, status: :not_acceptable
+      end
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
+  end
+
+  # Update existant Hall by id
+  def update_hall
+    if admin?
+      hall = Hall.find(update_params[:hall_id])
+      raise ActiveRecord::RecordNotFound unless hall
+
+      hall.name = update_params[:name]
+      hall.description = update_params[:description]
+      hall.cost = update_params[:cost]
+      hall.capacity = update_params[:capacity]
+      hall.image = update_params[:image]
+      if hall.save
+        render json: { updated: hall }
+      else
+        render json: { error: 'Bad Request' }, status: :not_acceptable
+      end
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  # Check if hall have any confirmed reservation
+  def check_reservations(hall)
+    @check = Reservation.where(hall_id: hall.id, status: 'Confirmed')
+    if @check.blank?
+      if hall.destroy
+        render json: { message: "#{hall.name} Hall (ID# #{hall.id}) has been deleted" }
+      else
+        render json: { message: 'Hall Cannot be deleted : 500 SERVER ERROR' }, status: :not_implemented
+      end
+    else
+      render json: { message: 'You Cannot Delete a Hall with a confirmed reservation' }, status: :unprocessable_entity
+    end
+  end
+
+  def delete_params
+    params.permit(:hall_id)
+  end
+
+  def update_params
+    params.permit(:hall_id, :name, :capacity, :cost, :description, :image)
+  end
+
+  def create_params
+    params.permit(:name, :capacity, :cost, :description, :image)
+  end
 end
