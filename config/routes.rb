db27196b3d1b9db974ddeedbfa7b3ccc6bf09c41
Lekan <jimoridwanlekan@gmail.com>
2022-09-19 10:45:22 +0100

    Add routes for creating reservation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   get "/authorized", to: "sessions#show"
+  post "/reservation", to: "reservations#create"
 
   #Admin Routes 
    #Pending Reservations Routes
