ab63b417c7d83876e0af7a61db6e02f341069986
Lekan <jimoridwanlekan@gmail.com>
2022-09-20 10:05:10 +0100

    Update users serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :role, :email
+  has_many :hall, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  
 end
