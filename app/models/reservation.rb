3d37a6eb8425c63ab662ad5075c5b3cb7998c250
Lekan <jimoridwanlekan@gmail.com>
2022-09-20 13:07:00 +0100

    Update models destroy

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :hall
   belongs_to :user
-  has_many :notifications, class_name: 'Notification', foreign_key: :reserve_id, dependent: :delete_all
+  has_many :notifications, class_name: 'Notification', foreign_key: :reserve_id, dependent: :destroy
   validates :reserve_date, presence: true
   validates :status, presence: true, length: { maximum: 20 }
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :role, :email
-  has_many :hall, dependent: :destroy
-  has_many :reservations, dependent: :destroy
+  has_many :hall
+  has_many :reservations
   
 end
