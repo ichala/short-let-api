d0a8245e60aca1d507eddfb753032526c15a20d4
Aimal Amiri <meaimal@hotmail.com>
2022-09-21 19:32:02 +0430

    Add profile update api

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,18 @@
 class ProfileController < ApplicationController
+  def update
+    if logged_in?
+      user = current_user
+      if user.update!(profile_params)
+        render json: { updated: current_user }
+      end
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def profile_params
+    params.permit(:first_name, :last_name, :email, :password)
+  end
 end
