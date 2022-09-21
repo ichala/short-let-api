fc9da8c5bf3325b7eb1b08b6af55216190952e2d
Aimal Amiri <meaimal@hotmail.com>
2022-09-21 19:53:59 +0430

    Fix the issue of updating a duplicate email for the user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,8 +1,11 @@
 class ProfileController < ApplicationController
   def update
     if logged_in?
-      user = current_user
-      render json: { updated: current_user } if user.update!(profile_params)
+      if current_user.update(profile_params)
+        render json: { updated: current_user }
+      else
+        render json: { error: current_user }, status: :not_implemented
+      end
     else
       render json: { error: 'Not Allowed' }, status: :unauthorized
     end
