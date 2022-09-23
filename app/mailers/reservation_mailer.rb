d041c32deb8833948e4e13dbe739c85f7ab566ef
Aimal Amiri <meaimal@hotmail.com>
2022-09-23 17:26:31 +0430

    Add notify to ReservationMailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,5 +1,9 @@
 class ReservationMailer < ApplicationMailer
   def notify(user, message)
-
+    @message = message
+    mail(
+      to: user.email,
+      subject: "Update on your reservation!"
+    )
   end
 end
