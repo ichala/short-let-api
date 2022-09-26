2508adf1498bd8bf5f44b1a663531ea6e361805f
Ali Jendoubi <alijandhd@gmail.com>
2022-09-26 10:03:10 +0100

    add rswag mount

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   #Public Routes
 
   post "/signup", to: "users#create"
